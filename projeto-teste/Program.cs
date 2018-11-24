20c62717b607d761eb4ac35e6551dfad24022a8c
Vinicius Ramos <viniciusrsss@gmail.com>
2018-11-24 13:49:28 -0200

    desejando um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -5,6 +5,7 @@ namespace projeto_teste {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Boa tarde");
+            Console.WriteLine("Tenha um bom dia!");
 
             Console.ReadLine();
         }
